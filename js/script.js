a006d783456d6f7eb7b86af7e4709ee0e1c62015
tqbrandao <tqbrandao@gmail.com>
2022-07-19 12:39:29 -0300

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,62 @@
+// Mobile Navigation
+
+// const mainNav = document.querySelector(".main-nav");
+const headerEl = document.querySelector(".header");
+const btnMobileEl = document.querySelector(".btn-mobile-nav");
+
+btnMobileEl.addEventListener("click", () => {
+  headerEl.classList.toggle("nav-open");
+});
+
+// ANIMACAO SUAVE NO SCROLL
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll voltando pro topo
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // Scroll para outros links
+    if (!href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Fechar navegacao mobile
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
+
+// ------------ NAVEGACAO STICKY ----------------
+
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+    if (!ent.isIntersecting) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // Dentro da viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
